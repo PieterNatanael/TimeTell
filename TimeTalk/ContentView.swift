9ef8b9d859466d8dae25145529c3511943fe70f0
PieterNatanael <pieternatanael@icloud.com>
2024-03-07 16:13:21 +0800

    Initial Commit

---
+++ TimeTalk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeTalk
+//
+//  Created by Pieter Yoshua Natanael on 07/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeTalk/TimeTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTalkApp.swift
+//  TimeTalk
+//
+//  Created by Pieter Yoshua Natanael on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
