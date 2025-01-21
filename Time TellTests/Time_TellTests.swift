a4e9e7d111d8f47ade31f526b1821a6b1bbda478
PieterNatanael <pieternatanael@icloud.com>
2025-01-21 16:34:39 +0800

    [Feature] unit testing bundle

---
+++ Time TellTests/Time_TellTests.swift
@@ -0,0 +1,16 @@
+//
+//  Time_TellTests.swift
+//  Time TellTests
+//
+//  Created by Pieter Yoshua Natanael on 21/01/25.
+//
+
+import Testing
+
+struct Time_TellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
